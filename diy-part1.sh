c499b193d2ed400c7f17bb79ba9148e5fdef4603
kjrzxu <xhb1011@163.com>
2022-12-11 17:03:45 +0800

    add more files

---
+++ diy-part1.sh
@@ -0,0 +1,2 @@
+#修改feeds.conf.default
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/' openwrt/lede/target/linux/ramips/Makefile
---
+++ diy-part2.sh
@@ -0,0 +1,19 @@
+#更改内核版本5.4为5.10
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/' openwrt/lede/target/linux/ramips/Makefile
+
+#添加menuconfig默认插件
+
+#修改界面主页分类和顺序
+
+#添加界面主页分类和顺序
+
+#替换极路由B70的hant驱动文件
+rm -rf openwrt/target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+rm -rf openwrt/linux/ramips/mt7621/base-files/etc/board.d/02_network
+cp build/LEDE-hnat-02network-dst/mt7621_hiwifi_hc5962.dts openwrt/target/linux/ramips/dts
+cp build/LEDE-hnat-02network-dst/02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d
+
+#修改插件名称并调整顺序
+sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/' openwrt/lede/target/linux/ramips/Makefile
+
+#修改插件配置文件
