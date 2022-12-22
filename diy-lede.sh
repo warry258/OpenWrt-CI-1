32d5ac31cc9340742d5c19217d6ecdeb874d3989
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2022-12-23 07:11:03 +0800

    更新 diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 #修改feeds.conf.default
-sed -i 's/fw876/kjrzxu/' openwrt/feeds.conf.default
 sed -i '/^#.*helloworld/s/^#//' openwrt/feeds.conf.default
 sed -i '/helloworld/a src-git package https://github.com/kjrzxu/package.git' openwrt/feeds.conf.default
 
