2df554df034d542723d799fb960e037180aa4e91
kjrzxu <xhb1011@163.com>
2022-12-22 21:02:00 +0800

    add more files

--- diy-luci.sh
+++ diy-luci.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
+#添加界面主页IPTV分类和顺序
+sed -i '/"VPN"/a entry({"admin", "iptv"}, firstchild(), _("IPTV"), 45).index = true' openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
+sed -i '/"VPN"/s/$/\n/' openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
+sed -i '/"IPTV"/s/^/\t/' openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
 
-#更改内核版本5.4为5.10
-sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/' openwrt/target/linux/ramips/Makefile
-
-#添加menuconfig默认插件
+# 把bootstrap替换成argon为源码必选主题
+sed -i 's/bootstrap/argon/ig' openwrt/feeds/luci/collections/luci/Makefile
 
 #修改界面主页分类和顺序
 
-
-#添加界面主页分类和顺序
-sed -i '/"vpn"/a entry({"admin", "iptv"}, firstchild(), _("IPTV"), 45).index = true' openwrt/feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/index.lua
-
-#替换极路由B70的hant驱动文件
-rm -rf openwrt/target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
-rm -rf openwrt/linux/ramips/mt7621/base-files/etc/board.d/02_network
-cp build/LEDE-hnat-02network-dst/mt7621_hiwifi_hc5962.dts openwrt/target/linux/ramips/dts
-cp build/LEDE-hnat-02network-dst/02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d
-
 #修改插件名称并调整顺序
-sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/' openwrt/lede/target/linux/ramips/Makefile
 
 #修改插件配置文件
