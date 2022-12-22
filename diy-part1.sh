a9e69db2b3cdaf852add5b7a1b83a859b0543f79
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2022-12-22 11:02:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,3 +6,7 @@ sed -i '/helloworld/a src-git package https://github.com/kjrzxu/package.git' ope
 #修改autosamba
 sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/Makefile
 sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/files/20-smb
+
+#修改automount
+rm -rf openwrt/package/lean/automount/files/15-automount
+cp build/15-automount openwrt/package/lean/automount/files
