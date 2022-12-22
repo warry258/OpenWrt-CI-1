a6c27dc359cf277ee0ad27c02d546f199157459e
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2022-12-22 10:33:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,3 +3,6 @@ sed -i 's/fw876/kjrzxu/' openwrt/feeds.conf.default
 sed -i '/^#.*helloworld/s/^#//' openwrt/feeds.conf.default
 sed -i '/helloworld/a src-git package https://github.com/kjrzxu/package.git' openwrt/feeds.conf.default
 
+#修改autosamba
+sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/Makefile
+sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/files/20-smb
