994d731d52675ff1f608622fb728fd236d117f96
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2022-12-28 18:40:08 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -9,11 +9,11 @@ sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/Makefile
 sed -i 's/samba4/samba/g' openwrt/package/lean/autosamba/files/20-smb
 
 #替换automount
-rm -rf openwrt/package/lean/automount/files/15-automount
-cp -r build/15-automount openwrt/package/lean/automount/files
+#rm -rf openwrt/package/lean/automount/files/15-automount
+#cp -r build/15-automount openwrt/package/lean/automount/files
 
 #修改默认插件
 sed -i 's/luci-app-filetransfer //' openwrt/include/target.mk
 sed -i '/ca-certificates/s/$/ \\/' openwrt/include/target.mk
-sed -i '/ca-certificates/s/$/\n/' openwrt/include/target.mk
 sed -i '/ca-certificates/r build/target.txt' openwrt/include/target.mk
+sed -i '/ca-certificates/s/$/\n/' openwrt/include/target.mk
