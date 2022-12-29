c7ebd11d9f41f37dbb2240b2553e98bfdf0ea1d2
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2022-12-29 13:39:40 +0800

    更新 diy-luci.sh

--- diy-luci.sh
+++ diy-luci.sh
@@ -78,7 +78,7 @@ sed -i 's/"网络存储"/"存储"/' openwrt/feeds/luci/applications/luci-app-vsf
 
 sed -i 's/"services"/"control"/g' openwrt/feeds/luci/applications/luci-app-wol/luasrc/controller/wol.lua
 
-sed -i 's/_("ZeroTier"), 99/"ZeroTier"), 120/' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
-sed -i 's/"vpn", "zerotier"/"services", "zerotier"/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+#sed -i 's/_("ZeroTier"), 99/"ZeroTier"), 120/' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
+#sed -i 's/"vpn", "zerotier"/"services", "zerotier"/g' openwrt/feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
 
 #修改配置文件
