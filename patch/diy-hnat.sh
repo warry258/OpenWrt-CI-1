b9d3f7f944ae7bf30878cfadea3123cff3d995c9
kjrzxu <119322320+kjrzxu@users.noreply.github.com>
2023-01-05 07:56:33 +0800

    Create diy-hnat.sh

---
+++ patch/diy-hnat.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 替换交换机hnat交换机文件
+rm -rf openwrt/target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
+rm -rf openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+cp build/LEDE-hnat-02network-dst/mt7621_hiwifi_hc5962.dts openwrt/target/linux/ramips/dts
+cp build/LEDE-hnat-02network-dst/02_network openwrt/target/linux/ramips/mt7621/base-files/etc/board.d
